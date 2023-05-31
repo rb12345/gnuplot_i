f711928b01e78d93dcf2eaa334dbd7954179e9d2
Long Radix <74498097+longradix@users.noreply.github.com>
2023-05-31 11:25:23 +0200

    Update animation.c
    * Updates based on testing

--- examples/animation.c
+++ examples/animation.c
@@ -1,3 +1,10 @@
+/*
+ * Examples of gnuplot_i.c usage
+ * Compilation: gcc -Wall -g animation.c gnuplot_i.c -o animation
+ *
+ * This program produces an animated sine wave in a gnuplot window
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "gnuplot_i.h"
@@ -14,7 +21,7 @@ int main(int argc, char *argv[]) {
     gnuplot_cmd(handle, "plot sin(x+%g)", phase);
   }
 
-  for (phase = 10; phase> = 0.1; phase -= 0.1) {
+  for (phase = 10; phase >= 0.1; phase -= 0.1) {
     gnuplot_resetplot(handle);
     gnuplot_cmd(handle, "plot sin(x+%g)", phase);
   }
